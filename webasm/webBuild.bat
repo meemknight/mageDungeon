f0ee5fbf15fe842b86ffc9007499c6944a1c6fce
vlod <vladalex420@gmail.com>
2026-01-08 23:54:25 +0200

    small fix

--- webasm/webBuild.bat
+++ webasm/webBuild.bat
@@ -1,28 +1,11 @@
 @echo off
 setlocal
+
 cd ..
 
-echo === Entering Emscripten environment ===
 call emsdk_env.bat
-if errorlevel 1 goto fail
-
-echo.
-echo === Configuring WebAssembly (Release) ===
-emcmake cmake -S . -B build-web -G Ninja -DCMAKE_BUILD_TYPE=Release
-if errorlevel 1 goto fail
-
-echo.
-echo === Building ===
+call emcmake cmake -S . -B build-web -G Ninja -DCMAKE_BUILD_TYPE=Release
 cmake --build build-web
-if errorlevel 1 goto fail
 
-echo.
-echo ===== BUILD SUCCESS =====
 pause
-exit /b 0
-
-:fail
-echo.
-echo ===== BUILD FAILED =====
-pause
-exit /b 1
+endlocal
